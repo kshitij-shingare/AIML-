727c2bf46d9de7897827e703b41c4a31ec25b4d3
kshitij-shingare <kshitijshingare49@gmail.com>
2026-01-18 01:05:48 +0530

    Day 2 Completed

--- Day 2/temp.py
+++ Day 2/temp.py
@@ -1,13 +1,11 @@
 # IF you want to run the assignment Codes You Can Use this.
 
-i = 1
-
-while i<=10:
-    
-    if(i % 3 == 0):
-        i+=1
-        continue
-    print(i)
-    i+=1
-    
-    
+def f (a):
+    fact =1
+    for i in range(1,a+1):
+        fact *=i
+    print(fact)
+a = int(input("enter the n value"))
+f(a)
+        
+ 
